b55d47c7f777223f7224ee3b6aaa65cb839cc369
ShayanJamil519 <shayanjamil500@gmail.com>
2023-12-27 10:17:47 +0500

    New Module: Blogs Added

--- src/pages/Blog/BlogMain.jsx
+++ src/pages/Blog/BlogMain.jsx
@@ -1,43 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Blog.css";
 import blogs from "./Blogdata";
+import { apiUrl } from "../../actions/apiUrl";
+import axios from "axios";
 
 const BlogMain = () => {
+  const [allBlogs, setAllBlogs] = useState([]);
+
+  useEffect(() => {
+    const getAllBlogs = async () => {
+      const response = await axios.get(`${apiUrl}/api/v1/blogs`);
+      console.log("response?.data?.data");
+      console.log(response?.data?.blogs);
+      setAllBlogs(response?.data?.blogs);
+    };
+
+    getAllBlogs();
+  }, []);
 
   return (
     <>
       <div className="blog-head">
-
-        <h3
-          className="blog-heading"
-          style={{ color: "wheat"}}
-        >
+        <h3 className="blog-heading" style={{ color: "wheat" }}>
           Read the <span className="yellow-color">Amazing Blogs</span> from Us!
         </h3>
       </div>
       <div className="bloglist">
-        {blogs.map((blog) => {
-          return (
-            <div className="blogPreview" key={blog.id}>
-              <Link to={`/Blog/Blogdata/${blog.id}`}>
-                
-              <div className="blogImages">
-                <img src={blog.img} alt="" />
+        {allBlogs.length > 0 &&
+          allBlogs.map((blog) => {
+            return (
+              <div className="blogPreview" key={blog.id}>
+                <Link to={`/Blog/Blogdata/${blog?._id}`}>
+                  <div className="blogImages">
+                    <img src={blogs[0].img} alt="logo" />
+                  </div>
+
+                  <div className="blogInfo">
+                    <div>
+                      <img
+                        className="blog-verify"
+                        src="/verification.jpg"
+                        alt="verification"
+                      />
+                    </div>
+                    <h2 className="blog-title">{blog?.name}</h2>
+                    <p className="blog-para">Written by {blog?.author}</p>
+                  </div>
+                </Link>
               </div>
-              
-              <div className="blogInfo">
-              <div >
-                  <img className="blog-verify" src="/verification.jpg" alt="verification" />
-                </div>
-              <h2 className="blog-title">{blog.title}</h2>
-              <p className="blog-para">Written by {blog.author}</p>
-              </div>
-              
-              </Link>
-            </div>
-          );
-        })}
+            );
+          })}
       </div>
     </>
   );
--- src/pages/Blog/BlogViewDetails.js
+++ src/pages/Blog/BlogViewDetails.js
@@ -1,40 +1,44 @@
-import React from 'react'
-import blogs from './Blogdata'
+import React, { useEffect, useState } from "react";
+import blogs from "./Blogdata";
 import "./Blog.css";
-import { useParams } from 'react-router-dom';
-
-
+import { useParams } from "react-router-dom";
+import { apiUrl } from "../../actions/apiUrl";
+import axios from "axios";
 
 const BlogViewDetails = () => {
-    const { id } = useParams();
-    const { title, author, body, img } = blogs[id];
+  const { id } = useParams();
+  const [blogDetails, setGetBlogDetails] = useState({});
 
+  useEffect(() => {
+    const getAllBlogs = async () => {
+      const response = await axios.get(`${apiUrl}/api/v1/blog/${id}`);
+      console.log("response?.data?.data");
+      console.log(response?.data?.blog);
+      setGetBlogDetails(response?.data?.blog);
+    };
 
+    getAllBlogs();
+  }, []);
 
-
-
-
-    return (
-        <div className='BlogViewDetails' key={id}>
-            <article>
-                <div className="blogViewHeader">
-                    <div className="blogViewHeadInfo">
-                        <h1>{title}</h1>
-                        <h2>Written By{author}</h2>
-                    </div>
-                    <div className="blogViewHeadImg">
-                        <img src={img} alt="img"></img>
-
-                    </div>
-                </div>
-
-                <div className='blogParadetails'><p>{body}</p></div>
-            </article>
-
+  return (
+    <div className="BlogViewDetails" key={id}>
+      <article>
+        <div className="blogViewHeader">
+          <div className="blogViewHeadInfo">
+            <h1>{blogDetails?.name}</h1>
+            <h2>Written By {blogDetails?.author}</h2>
+          </div>
+          <div className="blogViewHeadImg">
+            <img src={blogs[0].img} alt="img"></img>
+          </div>
         </div>
-    )
 
+        <div className="blogParadetails">
+          <p>{blogDetails?.content}</p>
+        </div>
+      </article>
+    </div>
+  );
+};
 
-}
-
-export default BlogViewDetails
+export default BlogViewDetails;
